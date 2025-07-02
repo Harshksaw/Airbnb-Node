dcc6266c89a5a27daf3fa63669364ac14c036f78
Harshksaw <mister.harshkumar@gmail.com>
2025-07-01 21:20:51 -0700

    feat: add SSL configuration to database connection options

--- HotelService/src/config/db.config.js
+++ HotelService/src/config/db.config.js
@@ -8,8 +8,18 @@ const config = {
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: 'mysql',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
   }
 }
+
+
+
 
 module.exports = config;
